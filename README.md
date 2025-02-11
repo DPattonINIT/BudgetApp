eddba40f96ca79b2fc9d8ac342e959634b8a068a
DPattonINIT <DlamarPatton@gmail.com>
2025-02-10 18:07:35 -0800

    update

---
+++ README.md
@@ -0,0 +1,13 @@
+In VS Code have the following at the top:
+
+// name 
+Darryl P
+ // Date Revised: 
+ 2/10/25 
+
+ // Exercise 
+ Budget APP 
+
+ // Brief Description of what you did. 
+
+ I made a budget app that allows users to set a budget, add and delete expenses, and track totals. Built with vanilla JavaScript, the app supports mobile use, imports/exports data, and updates amounts as changes are made.
--- obj/Debug/net8.0/BudgetApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BudgetApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1367715be9bd30c780cf88f811cad416089511c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
